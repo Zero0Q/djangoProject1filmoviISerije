a728c4eb6166d666168322ff00846fb70f243fac
A.M. <andrej2000aa@gmail.com>
2023-11-30 15:37:16 +0100

    cloudinary

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -136,8 +136,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
