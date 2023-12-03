5fe091afa06dc4653f78867f844476c63376b7a6
A.M. <andrej2000aa@gmail.com>
2023-12-03 13:00:04 +0100

    cloudinary

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -140,8 +140,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
@@ -153,10 +153,10 @@ cloudinary.config(
 )
 
 # # Base url to serve media files
-# MEDIA_URL = 'images/'
+MEDIA_URL = 'images/'
 #
 # # Path where media is stored
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
